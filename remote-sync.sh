ec89fd903570f5be8f19316cfad29ebe8be33bbc
armanpazouki <apazouki@gmail.com>
2015-06-24 15:22:07 -0500

    sync local and global

---
+++ remote-sync.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+USER=$1
+HOST=$2
+if [[ $USER == "" || $HOST == "" ]] ; then
+	echo "usage: `basename $0` user host"
+	exit 1
+fi
+shift 2
+
+LOCAL_DIR=~/Repos/Git/smarticles
+REMOTE_DIR=/home/pazouki/chronoStuff/smarticles
+
+# Generic way
+# REMOTE_DIR=`echo $LOCAL_DIR | sed -e "s/Users/home/"`
+
+SAFE_DIR=/home/arman/Repos/Git/smarticles
+if [[ $LOCAL_DIR/* != $SAFE_DIR/* ]] ; then
+	echo "`basename $0`: not under $SAFE_DIR, won't sync"
+	exit 1
+fi
+
+rsync --verbose --recursive --times \
+	--exclude ".*" --exclude "cmake/" --exclude "CMakeLists.txt" \
+		$LOCAL_DIR/ $USER@$HOST:$REMOTE_DIR
