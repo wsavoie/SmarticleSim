4666d1b2a43b6eeb63b52eb6336c53b2fd32e7cd
Will Savoie <wsavoie@gatech.edu>
2015-09-02 11:39:18 -0400

    changed flow rate matlab code for plotting more custom colors

--- matlabScripts/plotFlowRate.m
+++ matlabScripts/plotFlowRate.m
@@ -4,8 +4,7 @@ data=importdata(horzcat(directory_name,'\flowrate.txt'));
 
 dt = .0005;
 FS = 1/dt;
-
-cols = ['r','g','b','k','c','m'];
+cols = {[1,0,0],[1,.5,0],[113/255,188/255, 255/255],[0,0,0],[100,100,30],[30,27,95]};
 plotNames = {'Smarticles','Gait','U-Shape','Straight','Tetris','Vib at \circ','Vib Angle'};
 plotTypes = [1];
 figure(1);
@@ -31,19 +30,20 @@ gg= sortrows(gg,2);
 for i=1:size(gg,1)
     if i~=1
         if gg(i,2)==gg(i-1,2) %if true will mean a repeat in color on legend
-            plot([data(gg(i,1),1),data(gg(i,1),1)],[min(data(:,2)),max(data(:,2))],cols(gg(i,2)),'LineWidth',2)
+            plot([data(gg(i,1),1),data(gg(i,1),1)],[min(data(:,2)),max(data(:,2))],'color',cell2mat(cols(gg(i,2))),'LineWidth',2)
             continue;
         else
-            sp1 = [sp1 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(data(:,2)),max(data(:,2))],cols(gg(i,2)),'LineWidth',2)];
+            sp1 = [sp1 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(data(:,2)),max(data(:,2))],'color',cell2mat(cols(gg(i,2))),'LineWidth',2)];
         end
     else
-        sp1 = [sp1 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(data(:,2)),max(data(:,2))],cols(gg(i,2)),'LineWidth',2)];
+        sp1 = [sp1 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(data(:,2)),max(data(:,2))],'color',cell2mat(cols(gg(i,2))),'LineWidth',2)];
     end
 plotTypes=[plotTypes, (gg(i,2)+1)];
 end
 legend([sp1(1:end)],plotNames(plotTypes));
 axis([data(1,1) data(end,1) data(1,2),data(end,2)]);
-plotTypes
+ax = gca;
+ax.XTick= 0:1:max(ax.XTick)
 %% subplot 2
 subplot(2,1,2)
 hold on;
@@ -51,48 +51,50 @@ title('Flow Rate of Smarticles through hopper');
 % flowRate = diff(data(:,2))./diff(data(:,1));
 flowRate = diff(data(:,2))./diff(data(1:2,1)); 
 % flowRate = diff(data(:,2))./diff(data(:,1));
-plotNames = {'Flow Rate','Flow Rate Avged','Gait','Staple','Flat','Tetris','Vib at \circ','Vib Angle'};
-plotTypes2 = [1,2];
-sp2= [plot(data(1:end-1,1),flowRate)]; %plotting noisy regular differential
-window_size = 200;%average over 50 dts (
-simple = tsmovavg(flowRate,'s',window_size,1); %moving averaged differential
-sp2 =[sp2 plot(data(1:end-1,1),simple,'LineWidth',2)]; %
+plotNames = {'Flow Rate','Flow Rate Avged','Gait','U-Shape','Straight','Tetris','Vib at \circ','Vib Angle'};
+plotTypes2 = [2];
+sp2=[];
+% sp2= [plot(data(1:end-1,1),flowRate)]; %plotting noisy regular differential
 xlabel('Time(s)');
-ylabel('Flow Rate d(Smarticles)/{dt}');
+% ylabel('Flow Rate d(Smarticles)/{dt}');
+ylabel('Smarticle Flow Rate');
+
+
+hold on;
+n = 1; % designs an Nth order low pass
+timestep = .0005;
+f = 1/dt;
+% f = 10;
+we = f*2*pi*timestep;
+if we >1
+    we = .005; % .005*1/dt/2 5
+end
+[B,A] = butter(n,we);
+% filters the data in vector X with the filter
+%   described by vectors A and B to create the filtered data Y.  The
+%   filter is described by the difference equation:
+%
+%     a(1)*y(n) = b(1)*x(n) + b(2)*x(n-1) + ... + b(nb+1)*x(n-nb)
+%                           - a(2)*y(n-1) - ... - a(na+1)*y(n-na)
+ydat2 = filtfilt(B,A,flowRate);
+sp2=[sp2 plot(data(1:end-1,1),ydat2,'LineWidth',2)];
+
+
 for i=1:size(gg,1)
 if i~=1
     if gg(i,2)==gg(i-1,2) %if true will mean a repeat in color on legend
-        plot([data(gg(i,1),1),data(gg(i,1),1)],[min(flowRate),max(flowRate)],cols(gg(i,2)),'LineWidth',2)
+        plot([data(gg(i,1),1),data(gg(i,1),1)],[min(flowRate),max(flowRate)],'color',cell2mat(cols(gg(i,2))),'LineWidth',2)
          continue;
     else
-        sp2 = [sp2 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(flowRate),max(flowRate)],cols(gg(i,2)),'LineWidth',2)];
+        sp2 = [sp2 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(flowRate),max(flowRate)],'color',cell2mat(cols(gg(i,2))),'LineWidth',2)];
     end
 else
-    sp2 = [sp2 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(flowRate),max(flowRate)],cols(gg(i,2)),'LineWidth',2)];
+    sp2 = [sp2 plot([data(gg(i,1),1),data(gg(i,1),1)],[min(flowRate),max(flowRate)],'color',cell2mat(cols(gg(i,2))),'LineWidth',2)];
 end
 plotTypes2=[plotTypes2, (gg(i,2)+2)];
 end
 legend([sp2(1:end)],plotNames(plotTypes2));
-axis([data(1,1) data(end,1) min(flowRate) max(flowRate)]);
-
-
-%% power spectra
-figure(2);
-dt = .0005;
-Fs= 1/dt;
-t=0:dt:4;
-% x=flowRate(ggOld(5,1):ggOld(6,1));
-% x=simple((ggOld(4,1)+window_size):(ggOld(5,1)+window_size));
-% x=cos(0*2*pi*t)+ sin(2*2*pi*t);
-N=length(x);
-xdft=fft(x);
-xdft = xdft(1:N/2+1);
-psdx = (1/(Fs*N)) * abs(xdft).^2;
-psdx(2:end-1) = 2*psdx(2:end-1);
-freq = 0:Fs/length(x):Fs/2;
-% plot(freq,10*log10(psdx))
-plot(freq,10*log10(psdx))
-grid on
-title('Periodogram Using FFT')
-xlabel('Frequency (Hz)')
-ylabel('Power/Frequency (dB/Hz)')
+% axis([data(1,1) data(end,1) min(flowRate) max(flowRate)]);
+ axis([data(1,1) data(end,1) min(ydat2) max(ydat2)]);
+ax = gca;
+ax.XTick= 0:1:max(ax.XTick)
