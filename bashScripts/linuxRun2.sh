7150f2da89ac6ae18a3bd08a5a7766264ef907e3
wsavoie <wsavoie@gatech.edu>
2015-10-09 09:48:05 -0400

    updated bash script for linux

--- bashScripts/linuxRun2.sh
+++ bashScripts/linuxRun2.sh
@@ -19,22 +19,34 @@ re=$(grep -E "read" params | awk '{print $2}');
 pa=$(grep -E "pa" params | awk '{print $2}');
 echo "run vars!: $lw $dt $nl $re $pa"
 
-# lwArr=(0.01 1.1 1.3 1.4 0.8);
-# dtArr=(0.0005 0.0005 0.0005 0.0005 0.0005);
-# nlArr=(80 30 30 25 40);
-# lwArr=(0.1 0.3 0.5 0.7 0.9);
-# dtArr=(0.0005 0.0005 0.0005 0.0005 0.0005);
-# nlArr=(75 60 45 40 35);
-lwArr=(0.1 0.2 0.3 0.5 0.7 0.8 0.9 1.0);
-dtArr=(0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005);
-nlArr=(100 90 80 70 60 50 40 40);
-reArr=(0 0 0 0 0);
-paArr=(0 0 0 0 0);
+# lwArr=(0.1 0.2 0.3 0.5 0.7 0.8 0.9 1.0);
+# dtArr=(0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005);
+# nlArr=(100 90 80 70 60 50 40 40);
+# reArr=(0 0 0 0 0);
+# paArr=(0 0 0 0 0);
+#
+# # for i in `seq 0 4`; do
+# for i in `seq 0 7`; do
+#   echo $i
+#   a=./volFrac2/${lwArr[$i]}-$(date '+%Y%m%d-%H%M%S')
+#   mkdir $a
+#   cp smarticleMoves.csv $a
+#   cd $a
+#   $smartRunFile ${lwArr[$i]} ${dtArr[$i]} ${nlArr[$i]} ${reArr[$i]} ${paArr[$i]};
+#   cd ../..;
+# done;
+foldName=lowThickness
 
-# for i in `seq 0 4`; do
-for i in `seq 0 7`; do
+lwArr=(0.5 0.7 0.9 0.3 0.6 0.6 0.6 0.6 0.7 0.7 0.7 0.7);
+dtArr=(0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005);
+nlArr=(90 90 90 90 60 60 60 60 55 55 55 55);
+reArr=(0 0 0 0 0 0 0 0 0 0 0 0);
+paArr=(0 0 0 0 0 0 0 0 0 0 0 0);
+
+mkdir $foldName
+for i in `seq 0 3`; do
   echo $i
-  a=./Cyl4.4VolFracFilling/${lwArr[$i]}-$(date '+%Y%m%d-%H%M%S')
+  a=./$foldName/${lwArr[$i]}-${nlArr[$i]}-20-$(date '+%Y%m%d-%H%M%S')
   mkdir $a
   cp smarticleMoves.csv $a
   cd $a
@@ -42,13 +54,7 @@ for i in `seq 0 7`; do
   cd ../..;
 done;
 
-#
-# for i in `seq 0 4`; do
-#   echo $i
-#   $smartRunFile ${lwArr[$i]} ${dtArr[$i]} ${nlArr[$i]} ${reArr[$i]} ${paArr[$i]};
-#   mkdir ./MixedSmartWithOT10/r2/${paArr[$i]}/
-#   cp -R PostProcess ./MixedSmartWithOT10/r2/${paArr[$i]}
-# done
+
 
 echo "\\n blah \\n";
 echo ${lwArr[*]};
