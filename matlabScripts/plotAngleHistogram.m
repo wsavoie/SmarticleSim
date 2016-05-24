10798ce1c17dae5f643a4e785707906a5c495bff
Will Savoie <wsavoie@gatech.edu>
2016-05-24 10:23:30 -0400

    tried creating angular histogram, not completely polished yet, but it might not look as good in the end as the normal linear histogram

--- matlabScripts/plotAngleHistogram.m
+++ matlabScripts/plotAngleHistogram.m
@@ -5,7 +5,7 @@ runName = '-56-20160405-031112';
 ff = horzcat(mainFolder,runName);
 filename=horzcat(ff,'\PostProcess\Stress.txt');
 matName = '\PostProcess\stressData.mat';
-if exist(horzcat(ff,matName), 'file') == 2 
+if exist(horzcat(ff,matName), 'file') == 2 %file exists
     clear('x','simParams','smartPos','frameInfo','filename','file');
     ff =  horzcat(mainFolder,runName);
     load(horzcat(ff,matName));
@@ -24,41 +24,70 @@ cols = {[1,0,0],[1,.5,0],[.4431,.7373, 1],[0,0,0],[.392,.824,.118],[.7,.4,.7],[.
 
 figure(1);
 clf;
-ann=annotation('textbox', [0.6,0.8,0.1,0.1],...
-           'String', {horzcat('Smarticle Amt= 00'),'Current Directed Motion: 00'});
+% ann=annotation('textbox', [0.6,0.8,0.1,0.1],...
+%     'String', {horzcat('Smarticle Amt= 00'),'Current Directed Motion: 00'});
 
-skipFrames = simParams(3);       
+ann=annotation('textbox', [0.45,0.86,0.1,0.1],...
+    'String', {horzcat('Smarticle Amt= 00'),'Current Directed Motion: 00'});
+skipFrames = simParams(3);
 
 %%open video writer
 if(VID)
-    outputVideo = VideoWriter(fullfile('','histOut.avi'));
-    outputVideo.FrameRate=simParams(2);
-    open(outputVideo);
+    %     outputVideo = VideoWriter(fullfile('','histOut.avi'));
+    %     outputVideo.FrameRate=simParams(2);
+    %     open(outputVideo);
+    fold= [uigetdir,'\'];
 end
+
 for(i=1:size(smartPos,2))
     if(mod(i-1,skipFrames)==0) %-1 to allow first frame to be written
-        h1=histogram(smartPos{i}(:,1),60,'BinWidth',1);
-        set(gca,'xlim',[-120 120],'ylim',[0,12]);
+        %         h1=histogram(smartPos{i}(:,1),60,'BinWidth',1);
+        %         set(gca,'xlim',[-120 120],'ylim',[0,12]);
+        % h1.FaceColor = cell2mat(cols(moveType+1));
+        
+        %positions are -180 to 180,
+        %I want to have left side of map [90 270]->[-90 90] to be be angle 1
+        %
+        %and right side of map to be 
+        
+        [touta1,routa1]=rose(smartPos{i}(:,1),linspace(0,357,120));
+        [touta2,routa2]=rose(smartPos{i}(:,2),linspace(0,357,120));
+        
+        
         smartAmt = size(smartPos{i},1);
         moveType = (frameInfo(i,3));
         ann.String = {horzcat('Smarticle Amt=',num2str(smartAmt)),...
             horzcat('Current Directed Motion:',num2str(moveType)),...
             horzcat('Current Time: ',num2str(i*simParams(1)))};
-        xlabel('Angle (\circ)');
-        ylabel('Smarticle Number');
-        h1.FaceColor = cell2mat(cols(moveType+1));
+%         xlabel('Angle (\circ)');
+%         ylabel('Smarticle Number');
+        
+        subplot(1,2,1);
+        title('\alpha_1');
+        ang1=polarplot(touta1,routa1);
+%         ang1.Parent.ThetaTick=[0 30 60 90 120 150 180 -150 -120 -90 -60 -30];
+        ang1.Parent.ThetaLim=[-180 180];
+        ang1.Parent.RLim=[0 smartAmt];
+        subplot(1,2,2)
+        title('\alpha_2');
+        ang2=polarplot(touta2,routa2);
+        ang2.Parent.RLim=[0 smartAmt];
+%         ang2.Parent.ThetaTick=[0 30 60 90 120 150 180 -150 -120 -90 -60 -30];
+        ang2.Parent.ThetaLim=[-180 180];
         figText(gcf,13);
-        set(gcf, 'Position', [100, 100, 1280, 720]);
+        %         set(gcf, 'Position',ang2 [100, 100, 1280, 720]);
         if(VID)
-            writeVideo(outputVideo,getframe(gcf));
+            saveas(gcf,[fold, num2str(i)],'png')
+            %             writeVideo(outputVideo,getframe(gcf));
         else
             pause(0.001);
         end
-
+        
     end
 end
 if(VID)
-    close(outputVideo)
+    %     close(outputVideo)
+    system(['ffmpeg -framerate 4 -i ',fold,'%d.png -c:v mpeg4 -vtag xvid -r 30 -q 0',fold,'out.avi']);
 end
- %bottom left corner[x,y],[width to right,height up]
+%bottom left corner[x,y],[width to right,height up]
 % newAxis=axes('pos',[xpos,ypos), posz(2), posz(2)+sizez(2)]);
