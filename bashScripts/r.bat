781ba4ed9dde11f90d67314b493100348b418d1f
Will Savoie <wsavoie@gatech.edu>
2016-05-24 14:30:25 -0400

    most recent batch run file update (created a hard link)

--- bashScripts/r.bat
+++ bashScripts/r.bat
@@ -1,2 +1,11 @@
-::l/w dt    numlay    readfiles %Active ang1 ang2
-"D:\ChronoCode\chronoPkgs\SmarticlesBuild\Release\SmarticlesSystem_Dynamic.exe" .8 .00025 1 0 1 0 0 -66
+set LW=0.8
+set DT=.00025
+set NUMLAYER=6
+set READFILE=2
+set ACTIVEPCT=1
+set ANGLE1=0
+set ANGLE2=0
+set BOXANG=-25
+set NUMPERLAYER=5
+set LAZY=.00025
+start "SmarticlesSystem_Dynamic.exe" /high "D:\ChronoCode\chronoPkgs\SmarticlesBuild\Release\SmarticlesSystem_Dynamic.exe" %LW% %DT% %NUMLAYER% %READFILE% %ACTIVEPCT% %ANGLE1% %ANGLE2% %BOXANG% %NUMPERLAYER% %LAZY%
