9a0087499abb6e867409b767af874e92a676acd4
Will Savoie <wsavoie@gatech.edu>
2017-01-11 10:07:05 -0500

    removed good code from test and added it to newly and properly named file

---
+++ matlabScripts/readStressFileBatches.m
@@ -0,0 +1,16 @@
+path = 'A:\SmarticleRun\TestActive\';
+% path = 'D:\SimResults\Chrono\SmarticleU\tests\lazy .05\';
+a=dir(horzcat(path,'-*'));
+for i=1:length(a)
+    ff= horzcat(path,a(i).name,'\PostProcess\Stress.txt');
+    pts(ff);
+    matFile =  horzcat(path,a(i).name,'\PostProcess\stressData.mat');
+    if exist(matFile, 'file') == 2 
+        pts('matrix file already created');
+        continue
+    end
+   
+    readAllSmarticlesAngles(ff,0);
+end
+beep;
+beep;
