f418fa68647b8feaf36f949be1c1427bd9747499
Will Savoie <wsavoie@gatech.edu>
2017-04-28 08:42:51 -0400

    added in plotting functions for system with foam to rotate trajectory by the location of foam

--- matlabScripts/amoeba/multiMSD.m
+++ matlabScripts/amoeba/multiMSD.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 % load('D:\ChronoCode\chronoPkgs\Smarticles\matlabScripts\amoeba\smarticleExpVids\rmv3\movieInfo.mat');
 
-fold=uigetdir('D:\ChronoCode\chronoPkgs\Smarticles\matlabScripts\amoeba\smarticleExpVids\optinew\circle\1 inactive');
+fold=uigetdir('D:\ChronoCode\chronoPkgs\Smarticles\matlabScripts\amoeba\smarticleExpVids\');
 load(fullfile(fold,'movieInfo.mat'));
 figure(1)
 SPACE_UNITS = 'm';
@@ -22,13 +22,15 @@ fold
 %* 9. for each msd traj get linear fit of log
 %*10. partial msd fit with log
 %*11. for each msd traj get linear fit of log
+%*12. rotate each each track by the rotation of ring
+%*13. polar plot of rotation of ring (used for checking axes)
 %************************************************************
-showFigs=[2 10 11 ];
+showFigs=[7];
 
 ma = msdanalyzer(2, SPACE_UNITS, TIME_UNITS);
 
 %define curve params [] for all
-spk=[0]; smart=[]; gait=[1]; rob=[5]; v=[1:2,4:10];
+spk=[0]; smart=[]; gait=[1]; rob=[5]; v=[];
 
 props={spk smart gait rob v};
 inds=1;
@@ -56,8 +58,11 @@ if(isempty(ma.tracks))
 end
 
 %% 1 plot Y vs. X
-if(showFigs(showFigs==1))
-    hf1=figure(1); hax1=gca;
+xx=1;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
+    hax1=gca;
     ma.plotTracks
     ma.labelPlotTracks
     %     text(0,0+.01,'start')
@@ -87,11 +92,14 @@ if(showFigs(showFigs==1))
     
 end
 %% 2 plot MSD
-if(showFigs(showFigs==2))
-    hf2=figure(2); hax2=gca;
+xx=2;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
+    hax2=gca;
     ma = ma.computeMSD;
     ma.plotMeanMSD(gca, true);
-%     ma.plotMSD;
+    %     ma.plotMSD;
     [fo, gof]=ma.fitMeanMSD;
     % [a b]=ma.fitMeanMSD;
     D=fo.p1/2/ma.n_dim;
@@ -106,8 +114,10 @@ if(showFigs(showFigs==2))
     xlim([0 15]);
 end
 %% 3 plot vcorr
-if(showFigs(showFigs==3))
-    hf3=figure(3);
+xx=3;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
     hax3=gca;
     ma = ma.computeVCorr;
     ma.plotMeanVCorr
@@ -127,22 +137,28 @@ if(showFigs(showFigs==3))
     axis tight
 end
 %% 4 drift velocity
-if(showFigs(showFigs==4))
-    figure(4)
+xx=4;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
     ma = ma.computeDrift('velocity');
     ma.plotDrift;
     ma.labelPlotTracks;
 end
 %% 5 plot scatter data in x vs y where z axis is time
-if(showFigs(showFigs==5))
-    figure(5)
+xx=5;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
     alld=cell2mat(ma.tracks);
     scatter3(alld(:,2),alld(:,3),alld(:,1),'.');
     xlabel('x (m)'); ylabel('y (m)'); zlabel('t(s)');
 end
 %% 6 mean of position at each timestep for all tracks
-if(showFigs(showFigs==6))
-    figure(6)
+xx=6;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
     %get min track size
     idx=min(cellfun(@(x) size(x,1), ma.tracks(:)));
     idxDat=cellfun(@(x) x(1:idx,:),ma.tracks(:),'uniformoutput',false);
@@ -181,8 +197,9 @@ if(showFigs(showFigs==6))
 end
 
 %% 7 plot rotation
-if(showFigs(showFigs==7))
-    hf9=figure(7);
+xx=7;
+if(showFigs(showFigs==xx))
+    figure(xx)
     hold on;
     for(i=1:length(usedMovs))
         r=usedMovs(i).rot; t=usedMovs(i).t;
@@ -203,9 +220,10 @@ if(showFigs(showFigs==7))
     end
 end
 %% 8 put 3 mains into subplot
-if(showFigs(showFigs==8))
-    
-    hf10=figure(8);
+xx=8;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
     
     s1=subplot(2,2,1);
     pos=get(s1,'Position');
@@ -271,22 +289,21 @@ if(showFigs(showFigs==xx))
 end
 %% 10 partial msd fit with log
 xx=10;
-
 if(showFigs(showFigs==xx))
     figure(xx)
     hold on;
     for i=1:length(ma.msd)
-       
-    msd=ma.msd{i};
-    msd=msd(msd(:,1)<15&msd(:,1)>0.1,:);
-    
-    plot(msd(:,1),msd(:,2))
-    [lx]=log(msd(:,1));
-    [ly]=log(msd(:,2));
-    [f]=polyfit(lx,ly,1);
-    fs(i)=f(1);
-    [fo, gof] = fit(msd(:,1),msd(:,2), 'poly1', 'Weights', msd(:,3));
-%     fo.p1/4
+        
+        msd=ma.msd{i};
+        msd=msd(msd(:,1)<15&msd(:,1)>0.1,:);
+        
+        plot(msd(:,1),msd(:,2))
+        [lx]=log(msd(:,1));
+        [ly]=log(msd(:,2));
+        [f]=polyfit(lx,ly,1);
+        fs(i)=f(1);
+        [fo, gof] = fit(msd(:,1),msd(:,2), 'poly1', 'Weights', msd(:,3));
+        %     fo.p1/4
     end
     ma.labelPlotMSD
     [ma]=ma.fitMSD;
@@ -303,7 +320,7 @@ if(showFigs(showFigs==xx))
     cc.a;
     
     figText(gcf,15)
-%     msdanalyzer
+    %     msdanalyzer
 end
 %% 11 for each msd traj get linear fit of log
 xx=11;
@@ -338,7 +355,7 @@ if(showFigs(showFigs==xx))
     end
     plot(fs);
     
-     msd=ma.getMeanMSD;
+    msd=ma.getMeanMSD;
     msd=msd(msd(:,1)<15&msd(:,1)>0.1,:);
     [lx]=log(msd(:,1));
     [ly]=log(msd(:,2));
@@ -347,4 +364,61 @@ if(showFigs(showFigs==xx))
     
     pts('(*)mean of powers=',mean(fs),'  power of mean=',pom.p1,' meanpow=',meanPow);
     % std(fs);
+end
+
+%% 12 rotate each each track by the rotation of ring
+xx=12;
+if(showFigs(showFigs==xx))
+    figure(xx)
+    hold on;
+     figure(xx)
+    hold on;
+    hax1=gca;
+%     ma.plotTracks
+    ma.labelPlotTracks
+    %     text(0,0+.01,'start')
+    plot(0,0,'ro','markersize',8,'MarkerFaceColor','k');
+    y=get(gca,'ylim'); x=get(gca,'xlim');
+    c=max(abs(x)); xlim([-c,c]);
+    c=max(abs(y)); ylim([-c,c]);
+    axis equal
+    
+    axis([-.25 .25 -.25 .25]);
+    x=xlim; y=ylim;
+    set(gca,'xtick',[-.2:.1:.2],'ytick',[-.2:.1:.2]);
+    y=get(gca,'ylim'); x=get(gca,'xlim');
+    plot(x,[0,0],'r');
+    plot([0,0],y,'r');
+    for i=1:length(usedMovs)
+        % dpos=diff(pos);
+        % R = [cosd(theta(2:end)) -sind(theta(2:end)); sind(theta(2:end)) cosd(theta(2:end))];
+        pos=usedMovs(i).data{1}(:,2:3);
+        r=usedMovs(i).rot-pi/2;
+%         r=r-r(1);
+        dpos=[pos(1,:);diff(pos)];
+        newpos=dpos;
+        for(j=1:size(dpos,1))
+            R = [cos(r(j)) -sin(r(j)); sin(r(j)) cos(r(j))];
+            newpos(j,:)=R*dpos(j,:)';
+
+        end
+        
+        newpos=cumsum(newpos);
+        plot(newpos(:,1),newpos(:,2));
+        plot(newpos(end,1),newpos(end,2),'ko','markersize',4,'MarkerFaceColor','r');
+        figText(gcf,14)
+    end
+    
+end
+
+%% 13 polar plot of rotation of ring (used for checking axes)
+xx=13;
+if(showFigs(showFigs==xx))
+    figure(xx);
+    hold on;
+    for i=1:length(usedMovs)
+        r=linspace(0,1,length(usedMovs(i).rot));
+        polarplot(usedMovs(i).rot,r);
+    end
+    figText(gcf,14)
 end
