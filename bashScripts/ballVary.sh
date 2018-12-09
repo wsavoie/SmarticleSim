e896a2d8b86c8968bc06401e34dec908a64087de
Will Savoie <wsavoie@gatech.edu>
2018-12-09 14:25:12 -0500

    added new bash script files to run chrono param sweeps

---
+++ bashScripts/ballVary.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+smartRunFile="D:/ChronoCode/chronoPkgs/SmarticlesBuild/Release/SmarticlesSystem_Dynamic.exe"
+dataDir="D:/ChronoCode/chronoPkgs/Smarticles/data";
+##mf="B:\SmartSimResults\12_5"
+mf=/cygdrive/b/SmartS*/12*
+if diff ../Smarticles/data $dataDir;then #if no differences
+echo "found directory";
+else
+echo "copying directory";
+rm -r data
+cp -R $dataDir data
+fi
+
+
+#read from params file
+# lw=$(grep -E "lw" params | awk '{print $2}');
+# dt=$(grep -E "dt" params | awk '{print $2}');
+# nl=$(grep -E "numlayers" params | awk '{print $2}');
+# re=$(grep -E "read" params | awk '{print $2}');
+# pa=$(grep -E "pa" params | awk '{print $2}');
+echo "run vars!: $lw $dt $nl $re $pa"
+
+# lwArr=(0.1 0.2 0.3 0.5 0.7 0.8 0.9 1.0);
+# dtArr=(0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005 0.0005);
+# nlArr=(100 90 80 70 60 50 40 40);
+# reArr=(0 0 0 0 0);
+# paArr=(0 0 0 0 0);
+# for i in `seq 0 7`; dosg
+  # echo $i
+  # a=./volFrac2/${lwArr[$i]}-$(date '+%Y%m%d-%H%M%S')
+  # mkdir $a
+  # # cd $a
+  # $smartRunFile ${lwArr[$i]} ${dtArr[$i]} ${nlArr[$i]} ${reArr[$i]} ${paArr[$i]};
+  # cp -r ./PostProcess $a
+  # # cd ../..;
+# done;
+# echo "\\n blah \\n";
+# echo ${lwArr[*]};
+# # $smartRunFile $lw $dt $nl $re $pa
+#cd ../../../../../b/SmartS*/12*
+foldName=ballVary
+# lwArr=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9);
+# dtVal=0.0002
+# nl=(60 58 56 54 52 50 48 45 44)
+# npl=(15 14 13 12 11 10 9 8 8)
+# re=-1
+# reArr=(0 0 0 0 0 0 0 0 0 0 0 0);
+lwArr=0.7;
+dtVal=0.0002
+nl=30
+npl=10
+vibAmp=0
+re=-1
+pa=1
+ang1=90
+ang2=90
+vers=$1
+windx=0
+windy=0
+ss=0
+mkdir $foldName
+for i in `seq 0 0`; do
+  echo $i
+  mkdir ./$foldName/lw_${lwArr}_nl_${nl}_npl_${npl}_vib_${vibAmp[$i]}
+  a=$foldName/lw_${lwArr}_nl_${nl}_npl_${npl}_vib_${vibAmp[$i]}/v${vers}
+  mkdir $a
+  cp smarticleMoves.csv $a
+  cd $a
+  $smartRunFile ${lwArr} ${dtVal} ${nl} ${re} ${pa} ${ang1} ${ang2} 0 ${npl} 0.2 ${ss} 0 ${windx} ${windy} 1 ${vibAmp[$i]};
+  cd $mf
+  # cp -r ./PostProcess $a
+done;
